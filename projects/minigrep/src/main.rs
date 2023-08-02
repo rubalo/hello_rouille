95102627a7cc3137c1a37698874727b2950d4665
Gimeyochi <gimeyochi@gmail.com>
2023-08-02 11:37:14 +0200

    Using ! return type

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -8,10 +8,7 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    let config: Config = Config::build(&args).unwrap_or_else(| err | {
-        err_message(err);
-        process::exit(1);
-    });
+    let config: Config = Config::build(&args).unwrap_or_else(| err | err_message(err));
 
     println!("*****************");
     println!("Running {}", config.binary);
@@ -20,14 +17,14 @@ fn main() {
 
     if let Err(err) = minigrep::run(config) {
         err_message(&err.to_string());
-        process::exit(1);
     };
 
 }
 
-fn err_message(err: &str) { 
+fn err_message(err: &str) -> ! { 
     println!("{}", err);
     usage();
+    process::exit(1);
 }
 
 fn usage() {
